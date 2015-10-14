53bc60373d67e4229e575d94c9e6b1689d679bf9
unknown <taylor@Megatron.corp.bensonwood.com>
2015-10-13 20:57:48 -0400

    Started making inventory lists

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class Items {
+
+	public string itemName;
+	public int itemID;
+	public string itemDescription;
+	public Texture2D itemIcon;
+	public ItemType itemType;
+	public ItemTarget itemTarget;
+	
+	public enum ItemType { 
+		Consumable,
+		Quest
+	}
+	
+	public enum ItemTarget {
+		Self,
+		CurrentTarget,
+		AllEnemies
+	}
+	
+}
