1063ad2c163384d71552a777bfec7c3621be2a76
unknown <taylor@Megatron.corp.bensonwood.com>
2016-01-15 11:32:52 -0500

    Fixes to dissolving platforms

--- Assets/Scripts/Movement/DissolvingPlatformController.cs
+++ Assets/Scripts/Movement/DissolvingPlatformController.cs
@@ -23,13 +23,23 @@ public class DissolvingPlatformController : MonoBehaviour {
         boxCollider.enabled = !boxCollider.enabled;
     }
 
+    public void Waiting(){}
+
     public void OnTriggerEnter2D(Collider2D collider)
     {
+        float colliderTopPoint = GetComponent<Collider2D>().bounds.max.y;
+        float playerBottomPoint = collider.GetComponent<Collider2D>().bounds.min.y;
+        float differenceInColliders = Mathf.Abs(playerBottomPoint - colliderTopPoint);
         if (collider.gameObject.tag == "Player")
         {
-            //I need to somehow also check to see if I have been standing on the platform.
-            //I think i can do it by comparing the positions of both colliders.
-            StartDissolving();
+            if (differenceInColliders < .5)
+            {
+                Invoke("Waiting", 0.3f);
+                if (differenceInColliders < .5)
+                {
+                    StartDissolving();
+                }
+            }
         }
     }
 }
